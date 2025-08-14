22ade8c1e9d7d3cc5ccbcdafcf313faa9160c26a
Garin Curtis <g.curtis0220231@arts.ac.uk>
2025-08-14 19:53:20 +0100

    Updated (R) Evocations page

--- src/app/projects/ancestral-(r)evocations-tate-modern/page.jsx
+++ src/app/projects/ancestral-(r)evocations-tate-modern/page.jsx
@@ -31,10 +31,16 @@ export default function AncestralRevocations() {
   return (
     <ReactLenis root>
        <Header />
+
+
+      <div className={styles.text}>
+        <h1>Ancestral (R)evocations - Tate Modern</h1>
+      </div>
+
     <div className={styles.container}>
       <div className={styles.imageWrapper}>
         <Image
-          src="/images/ancestral picture.png"
+          src="/images/Erika_Tan_Ancestral.jpg"
           alt="Ancestral (R)evocations"
           width={2557}
           height={1595}
@@ -45,9 +51,7 @@ export default function AncestralRevocations() {
 
 
 
-      <div className={styles.text}>
-        <h1>Ancestral (R)evocations - Tate Modern</h1>
-      </div>
+
 
 
 
@@ -55,28 +59,26 @@ export default function AncestralRevocations() {
       <div ref={description} className={styles.description}>
             <div className={styles.body}>
                 <p>
-                {
-                    phrase.split(" ").map( (word, index) => {
-                        return <span key={index} className={styles.mask}><motion.span variants={slideUp} custom={index} animate={isInView ? "open" : "closed"}>{word}</motion.span></span>
-                    })
-                }
+                  Collaborated with artist Erika Tan to realise a semantic sound data sonification system as part of her Ancestral (R)evocations installation at Tate Modern’s Tanks (October 2024). I trained Ircam’s Realtime Audio Variational AutoEncoder (RAVE) on a custom dataset recorded on-site, and engineered an interactive machine learning-based audio system that enabled real-time manipulation of the model’s latent space. The system mapped archival metadata to generative audio parameters, integrated with granular synthesis in Pure Data, and was optimised for live gallery performance with stable responsiveness across hardware and software layers. This DIY diagnostic tool, comprising fragmented instruments and mechanised components, functioned as a live, hybrid intervention into ancestral and archival traces—bridging performance, sound, video, and computational labour. Tools used included Python, PyTorch, Pure Data, and OSC.
+
+
+                      <div className={styles.info}>
+                          <a href="https://www.tate.org.uk/research/tate-papers/36/interview-erika-tan" target="_blank" rel="noopener noreferrer">
+                            Museum-X-Machine-X-Me
+                          </a>
+                      </div>
+
                 </p>
-           
-                <div data-scroll data-scroll-speed={0.1}>
- 
-                </div>
             </div>
         </div>
 
 
 
-      <div className={styles.info}>
-        <a href="https://www.tate.org.uk/visit/tate-modern/display/tanks/museum-x-machine-x-me" target="_blank" rel="noopener noreferrer">
-        Museum-X-Machine-X-Me
-        </a>
-      </div>
 
 
+
+
+{/* 
         <div className={styles.videoWrapper}>
           <iframe
             className={styles.video}
@@ -88,13 +90,19 @@ export default function AncestralRevocations() {
             allowFullScreen
             title="Vimeo video"
           ></iframe>
-        </div>
+        </div> */}
 
 
 
 
         
         </div>  
+
+
+
+
+
+      
 
 
     <Footer/>
